6fc9baa6a7575658f19b1c8a67e996b32f7335fd
Valerie Bolden <valerie.bolden@gmail.com>
2026-05-21 14:29:21 -0400

    modified create note route to add authenticated users id

--- routes/api/noteRoutes.js
+++ routes/api/noteRoutes.js
@@ -24,6 +24,7 @@ router.post('/', async (req, res) => {
     const note = await Note.create({
       ...req.body,
       // The user ID needs to be added here
+      user: req.user._id,
     });
     res.status(201).json(note);
   } catch (err) {
